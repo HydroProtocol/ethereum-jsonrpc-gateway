8167167c055f917b4c288b4e0031e24619e5ebac
wanglei <wanglei@ddex.io>
2019-12-31 11:53:31 +0800

    add tests

---
+++ upstream_test.go
@@ -0,0 +1,6 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
---
+++ util_test.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestRandStringRunes(t *testing.T) {
+	str1 := randStringRunes(8)
+	assert.IsType(t, string, str1)
+	assert.Equal(t, 8, len(str1))
+
+	str1 := randStringRunes(64)
+	assert.IsType(t, string, str2)
+	assert.Equal(t, 64, len(str2))
+}
+
+func TestNoErrorFieldInJSON(t *testing.T) {
+	jsonStr1 := "{\"foo\": 100, \"bar\": \"Go\"}"
+	result1 := noErrorFieldInJSON(jsonStr1)
+	assert.Equal(t, true, result1)
+
+	jsonStr2 := "foobar"
+	result2 := noErrorFieldInJSON(jsonStr2)
+	assert.Equal(t, false, result2)
+
+	jsonStr3 := "{\"foo\": 100, \"error\": \"Go\"}"
+	result3 := noErrorFieldInJSON(jsonStr3)
+	assert.Equal(t, false, result3)
+}
