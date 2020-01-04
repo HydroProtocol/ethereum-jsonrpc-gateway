567e2bde4aa1a265ab6a51782d46e45e44db80b4
wanglei <wanglei@ddex.io>
2020-01-04 11:57:38 +0800

    merge test branch

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,13 @@
 FROM golang:1.13 as builder
 WORKDIR /app
 COPY . /app
-RUN go build -v -installsuffix cgo -ldflags '-s -w' -o /app/jsonrpc-gateway cmd/gateway.go
+RUN go build -v -installsuffix cgo -ldflags '-s -w' -o /app/main cmd/main.go
 
 FROM alpine
 RUN mkdir /lib64 && ln -s /lib/libc.musl-x86_64.so.1 /lib64/ld-linux-x86-64.so.2
 RUN apk --no-cache add ca-certificates
-COPY --from=builder /app/jsonrpc-gateway /app/jsonrpc-gateway
+COPY --from=builder /app/main /app/main
 COPY --from=builder /app/config.json /app/config.json
 RUN addgroup -S appuser && adduser -S -G appuser appuser
 USER appuser
-CMD ["/app/jsonrpc-gateway"]
+CMD ["/app/main"]
--- utils/utils_test.go
+++ utils/utils_test.go
@@ -6,25 +6,25 @@ import (
 )
 
 func TestRandStringRunes(t *testing.T) {
-	str1 := randStringRunes(8)
+	str1 := RandStringRunes(8)
 	assert.IsType(t, string, str1)
 	assert.Equal(t, 8, len(str1))
 
-	str1 := randStringRunes(64)
+	str1 := RandStringRunes(64)
 	assert.IsType(t, string, str2)
 	assert.Equal(t, 64, len(str2))
 }
 
 func TestNoErrorFieldInJSON(t *testing.T) {
 	jsonStr1 := "{\"foo\": 100, \"bar\": \"Go\"}"
-	result1 := noErrorFieldInJSON(jsonStr1)
+	result1 := NoErrorFieldInJSON(jsonStr1)
 	assert.Equal(t, true, result1)
 
 	jsonStr2 := "foobar"
-	result2 := noErrorFieldInJSON(jsonStr2)
+	result2 := NoErrorFieldInJSON(jsonStr2)
 	assert.Equal(t, false, result2)
 
 	jsonStr3 := "{\"foo\": 100, \"error\": \"Go\"}"
-	result3 := noErrorFieldInJSON(jsonStr3)
+	result3 := NoErrorFieldInJSON(jsonStr3)
 	assert.Equal(t, false, result3)
 }
