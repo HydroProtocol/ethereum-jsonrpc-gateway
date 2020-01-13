bf0370a94ccbe3ece07f9743234f4295cfa555cf
wanglei <wanglei@ddex.io>
2020-01-13 11:54:42 +0800

    fix fallback config

---
+++ core/config_test.go
@@ -0,0 +1,181 @@
+package core
+
+import (
+	"context"
+	"encoding/json"
+	"testing"
+
+	"github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBuildRunningConfigFromConfigNAIVE(t *testing.T) {
+	var testConfigStr1 = `{
+		"_upstreams": "support http, https, ws, wss",
+		"upstreams": [
+		  "https://ropsten.infura.io/v3/83438c4dcf834ceb8944162688749707"
+		],
+		
+		"_strategy": "support NAIVE, RACE, FALLBACK",
+		"strategy": "NAIVE",
+	  
+		"_methodLimitationEnabled": "limit or not",
+		"methodLimitationEnabled": true,
+	  
+		"_allowedMethods": "can be ignored when set methodLimitationEnabled false",
+		"allowedMethods": ["eth_blockNumber"],
+	  
+		"_contractWhitelist": "can be ignored when set methodLimitationEnabled false",
+		"contractWhitelist": []
+	  }`
+
+	config := &Config{}
+
+	err := json.Unmarshal([]byte(testConfigStr1), config)
+
+	currentRunningConfig, err := buildRunningConfigFromConfig(context.Background(), config)
+
+	if err != nil {
+		logrus.Fatal(err)
+	}
+
+	assert.Equal(t, true, currentRunningConfig.MethodLimitationEnabled)
+
+	var testConfigStr2 = `{
+		"_upstreams": "support http, https, ws, wss",
+		"upstreams": [
+		  "https://ropsten.infura.io/v3/83438c4dcf834ceb8944162688749707",
+		  "https://test1.com"
+		],
+		
+		"_strategy": "support NAIVE, RACE, FALLBACK",
+		"strategy": "NAIVE",
+	  
+		"_methodLimitationEnabled": "limit or not",
+		"methodLimitationEnabled": true,
+	  
+		"_allowedMethods": "can be ignored when set methodLimitationEnabled false",
+		"allowedMethods": ["eth_blockNumber"],
+	  
+		"_contractWhitelist": "can be ignored when set methodLimitationEnabled false",
+		"contractWhitelist": []
+	  }`
+
+	err = json.Unmarshal([]byte(testConfigStr2), config)
+
+	assert.Panics(t, func() { buildRunningConfigFromConfig(context.Background(), config) })
+}
+
+func TestBuildRunningConfigFromConfigRACE(t *testing.T) {
+	var testConfigStr1 = `{
+		"_upstreams": "support http, https, ws, wss",
+		"upstreams": [
+		  "https://ropsten.infura.io/v3/83438c4dcf834ceb8944162688749707",
+		  "https://test1.com"
+		],
+		
+		"_strategy": "support NAIVE, RACE, FALLBACK",
+		"strategy": "RACE",
+	  
+		"_methodLimitationEnabled": "limit or not",
+		"methodLimitationEnabled": true,
+	  
+		"_allowedMethods": "can be ignored when set methodLimitationEnabled false",
+		"allowedMethods": ["eth_blockNumber"],
+	  
+		"_contractWhitelist": "can be ignored when set methodLimitationEnabled false",
+		"contractWhitelist": []
+	  }`
+
+	config := &Config{}
+
+	err := json.Unmarshal([]byte(testConfigStr1), config)
+
+	currentRunningConfig, err := buildRunningConfigFromConfig(context.Background(), config)
+
+	if err != nil {
+		logrus.Fatal(err)
+	}
+
+	assert.Equal(t, true, currentRunningConfig.MethodLimitationEnabled)
+
+	var testConfigStr2 = `{
+		"_upstreams": "support http, https, ws, wss",
+		"upstreams": [
+		  "https://ropsten.infura.io/v3/83438c4dcf834ceb8944162688749707"
+		],
+		
+		"_strategy": "support NAIVE, RACE, FALLBACK",
+		"strategy": "RACE",
+	  
+		"_methodLimitationEnabled": "limit or not",
+		"methodLimitationEnabled": true,
+	  
+		"_allowedMethods": "can be ignored when set methodLimitationEnabled false",
+		"allowedMethods": ["eth_blockNumber"],
+	  
+		"_contractWhitelist": "can be ignored when set methodLimitationEnabled false",
+		"contractWhitelist": []
+	  }`
+
+	err = json.Unmarshal([]byte(testConfigStr2), config)
+
+	assert.Panics(t, func() { buildRunningConfigFromConfig(context.Background(), config) })
+}
+
+func TestBuildRunningConfigFromConfigFALLBACK(t *testing.T) {
+	var testConfigStr1 = `{
+		"_upstreams": "support http, https, ws, wss",
+		"upstreams": [
+		  "https://ropsten.infura.io/v3/83438c4dcf834ceb8944162688749707",
+		  "https://test1.com"
+		],
+		
+		"_strategy": "support NAIVE, RACE, FALLBACK",
+		"strategy": "FALLBACK",
+	  
+		"_methodLimitationEnabled": "limit or not",
+		"methodLimitationEnabled": true,
+	  
+		"_allowedMethods": "can be ignored when set methodLimitationEnabled false",
+		"allowedMethods": ["eth_blockNumber"],
+	  
+		"_contractWhitelist": "can be ignored when set methodLimitationEnabled false",
+		"contractWhitelist": ["0xc2c57336e01695D34F8012f6c0d250baB2Dd38Da"]
+	  }`
+
+	config := &Config{}
+
+	err := json.Unmarshal([]byte(testConfigStr1), config)
+
+	currentRunningConfig, err := buildRunningConfigFromConfig(context.Background(), config)
+
+	if err != nil {
+		logrus.Fatal(err)
+	}
+
+	assert.Equal(t, true, currentRunningConfig.MethodLimitationEnabled)
+
+	var testConfigStr2 = `{
+		"_upstreams": "support http, https, ws, wss",
+		"upstreams": [
+		  "https://ropsten.infura.io/v3/83438c4dcf834ceb8944162688749707"
+		],
+		
+		"_strategy": "support NAIVE, RACE, FALLBACK",
+		"strategy": "FALLBACK",
+	  
+		"_methodLimitationEnabled": "limit or not",
+		"methodLimitationEnabled": true,
+	  
+		"_allowedMethods": "can be ignored when set methodLimitationEnabled false",
+		"allowedMethods": ["eth_blockNumber"],
+	  
+		"_contractWhitelist": "can be ignored when set methodLimitationEnabled false",
+		"contractWhitelist": []
+	  }`
+
+	err = json.Unmarshal([]byte(testConfigStr2), config)
+
+	assert.Panics(t, func() { buildRunningConfigFromConfig(context.Background(), config) })
+}
--- core/rpcserver.go
+++ core/rpcserver.go
@@ -3,13 +3,11 @@ package core
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"math/rand"
 	"net/http"
 	"os"
 	"os/signal"
-	"strings"
 	"syscall"
 	"time"
 
@@ -40,26 +38,6 @@ func createHTTPClient() *http.Client {
 }
 
 type Server struct{}
-
-type Config struct {
-	Upstreams               []string `json:"upstreams"`
-	Strategy                string   `json:"strategy"`
-	MethodLimitationEnabled bool     `json:"methodLimitationEnabled"`
-	AllowedMethods          []string `json:"allowedMethods"`
-	ContractWhitelist       []string `json:"contractWhitelist"`
-}
-
-type RunningConfig struct {
-	ctx                     context.Context
-	stop                    context.CancelFunc
-	Upstreams               []Upstream
-	Strategy                IStrategy
-	MethodLimitationEnabled bool
-	allowedMethods          map[string]bool
-	allowedCallContracts    map[string]bool
-}
-
-var currentRunningConfig *RunningConfig
 
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
@@ -175,72 +153,6 @@ func (h *Server) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 
 	_, _ = w.Write(bts)
 	logrus.Infof("Req%s from %s %s 200", isArchiveRequestText, req.RemoteAddr, proxyRequest.data.Method)
-}
-
-func buildRunningConfigFromConfig(parentContext context.Context, cfg *Config) (*RunningConfig, error) {
-	ctx, stop := context.WithCancel(parentContext)
-
-	rcfg := &RunningConfig{
-		ctx:  ctx,
-		stop: stop,
-	}
-
-	for _, url := range cfg.Upstreams {
-
-		// hack, refactor this sometime
-
-		var primaryUrl string
-		var oldTrieUrl string
-
-		if strings.Contains(url, ",") {
-			urls := strings.Split(url, ",")
-			primaryUrl = urls[0]
-			oldTrieUrl = urls[1]
-		} else {
-			primaryUrl = url
-			oldTrieUrl = url
-		}
-
-		rcfg.Upstreams = append(rcfg.Upstreams, newUpstream(ctx, primaryUrl, oldTrieUrl))
-	}
-
-	if len(rcfg.Upstreams) == 0 {
-		return nil, fmt.Errorf("need upstreams")
-	}
-
-	switch cfg.Strategy {
-	case "NAIVE":
-		if len(rcfg.Upstreams) > 1 {
-			panic(fmt.Errorf("naive proxy strategy require exact 1 upstream"))
-		}
-		rcfg.Strategy = newNaiveProxy()
-	case "RACE":
-		if len(rcfg.Upstreams) < 2 {
-			panic(fmt.Errorf("race proxy strategy require more than 1 upstream"))
-		}
-		rcfg.Strategy = newRaceProxy()
-	case "FALLBACK":
-		if len(rcfg.Upstreams) < 2 {
-			panic(fmt.Errorf("fallback proxy strategy require more than 1 upstream"))
-		}
-		rcfg.Strategy = newFallbackProxy()
-	default:
-		return nil, fmt.Errorf("blank of unsupported strategy: %s", cfg.Strategy)
-	}
-
-	rcfg.MethodLimitationEnabled = cfg.MethodLimitationEnabled
-
-	rcfg.allowedMethods = make(map[string]bool)
-	for i := 0; i < len(cfg.AllowedMethods); i++ {
-		rcfg.allowedMethods[cfg.AllowedMethods[i]] = true
-	}
-
-	rcfg.allowedCallContracts = make(map[string]bool)
-	for i := 0; i < len(cfg.ContractWhitelist); i++ {
-		rcfg.allowedCallContracts[strings.ToLower(cfg.ContractWhitelist[i])] = true
-	}
-
-	return rcfg, nil
 }
 
 func waitExitSignal(ctxStop context.CancelFunc) {
